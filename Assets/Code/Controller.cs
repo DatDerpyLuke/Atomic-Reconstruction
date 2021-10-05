50b2ab23b1009e1f1f309ceb850b72ae3b234bba
DatDerpyLuke <dat.derpiest.luke@gmail.com>
2021-10-05 19:38:18 -0300

    Episode 2 and 3

--- Assets/Code/Controller.cs
+++ Assets/Code/Controller.cs
@@ -4,38 +4,76 @@ using UnityEngine.UI;
 
 public class Controller : MonoBehaviour
 {
-    public int quarks;
+    public double quarks;
     public TMP_Text Qtext;
 
-    public int electrons;
+    public double electrons;
     public TMP_Text Etext;
 
-    public int pn;
+    public double pn;
     public TMP_Text PNtext;
+
+    public double hydrogen;
+    public TMP_Text Hydrogentext;
+
+    public double quarkMultiplier = 1;
+    public double elecMultiplier = 1;
+    public double pnMultiplier = 1;
+
+    public GameObject Clicky;
 
     private void Update()
     {
         Qtext.text = $"{quarks} quarks";
         Etext.text = $"{electrons} electrons";
         PNtext.text = $"{pn} protons/neutrons";
+        Hydrogentext.text = $"{hydrogen} hydrogen atoms";
     }
 
     public void Qclick()
     {
-        quarks += 1;
+        quarks += 1 * quarkMultiplier;
     }
 
     public void Eclick()
     {
-        electrons += 1;
+        electrons += 1 * elecMultiplier;
     }
 
     public void PNmake()
     {
         if (quarks >= 3)
         {
-            pn += 1;
+            pn += 1 * pnMultiplier;
             quarks -= 3;
+            quarkMultiplier += 0.5;
+        }
+    }
+
+    public void Hmake()
+    {
+        if (pn >= 1 && electrons >= 1)
+        {
+            hydrogen += 1;
+            pn -= 1;
+            electrons -= 1;
+            elecMultiplier += 0.5;
+            pnMultiplier += 0.5;
+        }
+    }
+
+    public void ChangeMenus(int switcher)
+    {
+        switch (switcher)
+        {
+            case 1:
+                Clicky.SetActive(true);
+                break;
+            case 99:
+                Debug.Log("Hi, how are you?");
+                break;
+            default:
+                break;
         }
     }
 }
