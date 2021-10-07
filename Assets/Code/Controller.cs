162b6b411b541a8b80132867335c49e580f79f30
DatDerpyLuke <dat.derpiest.luke@gmail.com>
2021-10-07 16:54:28 -0300

    Episode 5 reached!

--- Assets/Code/Controller.cs
+++ Assets/Code/Controller.cs
@@ -16,18 +16,29 @@ public class Controller : MonoBehaviour
     public double hydrogen;
     public TMP_Text Hydrogentext;
 
+    public double helium;
+    public TMP_Text Heliumtext;
+
+    public double carbon;
+    public TMP_Text Carbontext;
+
     public double quarkMultiplier = 1;
     public double elecMultiplier = 1;
     public double pnMultiplier = 1;
+    public double hydrogenMultiplier = 1;
+    public double heliumMultiplier = 1;
 
     public GameObject Clicky;
+    public GameObject SHeavy;
 
     private void Update()
     {
-        Qtext.text = $"{quarks} quarks";
-        Etext.text = $"{electrons} electrons";
-        PNtext.text = $"{pn} protons/neutrons";
-        Hydrogentext.text = $"{hydrogen} hydrogen atoms";
+        Qtext.text = $"{quarks} quarks\n{quarkMultiplier}x";
+        Etext.text = $"{electrons} electrons\n{elecMultiplier}x";
+        PNtext.text = $"{pn} protons/neutrons\n{pnMultiplier}x";
+        Hydrogentext.text = $"{hydrogen} hydrogen atoms\n{hydrogenMultiplier}x";
+        Heliumtext.text = $"{helium} helium atoms\n{heliumMultiplier}x";
+        Carbontext.text = $"{carbon} carbon atoms";
     }
 
     public void Qclick()
@@ -54,11 +65,31 @@ public class Controller : MonoBehaviour
     {
         if (pn >= 1 && electrons >= 1)
         {
-            hydrogen += 1;
+            hydrogen += 1 * hydrogenMultiplier;
             pn -= 1;
             electrons -= 1;
             elecMultiplier += 0.5;
             pnMultiplier += 0.5;
+        }
+    }
+
+    public void Hemake()
+    {
+        if (hydrogen >= 4)
+        {
+            helium += 1 * heliumMultiplier;
+            hydrogen -= 4;
+            hydrogenMultiplier += 0.5;
+        }
+    }
+
+    public void Cmake()
+    {
+        if (helium >= 3)
+        {
+            carbon += 1;
+            helium -= 3;
+            heliumMultiplier += 0.75;
         }
     }
 
@@ -68,6 +99,11 @@ public class Controller : MonoBehaviour
         {
             case 1:
                 Clicky.SetActive(true);
+                SHeavy.SetActive(false);
+                break;
+            case 2:
+                Clicky.SetActive(false);
+                SHeavy.SetActive(true);
                 break;
             case 99:
                 Debug.Log("Hi, how are you?");
